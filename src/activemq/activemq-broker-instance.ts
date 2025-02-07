9976386cb6299eac343373ca1018fc176ea033f4
Witold Kowalik <wkowal@amazon.pl>
2025-02-07 09:55:20 +0100

    For Active MQ single instance, if more then one subnets has been selected pick first and annotate the fact in console

--- src/activemq/activemq-broker-instance.ts
+++ src/activemq/activemq-broker-instance.ts
@@ -2,7 +2,7 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
-import { Aws, Fn, Token } from "aws-cdk-lib";
+import { Aws, Fn, Token, Annotations } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { IActiveMqBroker } from "./activemq-broker";
 import {
@@ -12,6 +12,7 @@ import {
 import { ActiveMqBrokerEndpoints } from "./activemq-broker-endpoints";
 import { BrokerDeploymentMode } from "../broker-deployment-mode";
 import { StorageType } from "../storage-type";
+import { SubnetSelection } from "aws-cdk-lib/aws-ec2";
 
 export interface ActiveMqBrokerInstanceProps
   extends ActiveMqBrokerDeploymentProps {
@@ -48,10 +49,33 @@ export class ActiveMqBrokerInstance
     id: string,
     props: ActiveMqBrokerInstanceProps,
   ) {
-    super(scope, id, {
+
+
+		let subnetSelection = props.vpcSubnets
+
+		// check if subnet selection has been specified 
+		if(props.vpcSubnets){
+			const subnets = props.vpc?.selectSubnets(props.vpcSubnets);
+			
+			// if selection is valid for a vpc
+			if(subnets){
+				// single instance allows only one subnet take the first one
+				subnetSelection = { subnets: [subnets.subnets[0]]} as SubnetSelection;
+
+				if(subnets.subnets.length > 1)
+					// Annotate the fact of taking first one when more then one were selected
+					Annotations.of(scope).addWarning(
+						`Exactly 1 subnet in [SINGLE_INSTANCE] deployment mode is needed. vpcSubnets selection returned ${subnets.subnets.length}. Taking first one from the selection`,
+				);
+			}
+		}
+
+		super(scope, id, {
       ...props,
+			vpcSubnets: subnetSelection,
       deploymentMode: BrokerDeploymentMode.SINGLE_INSTANCE,
     });
+
 
     this.endpoints = {
       amqp: {
--- test/activemq/activemq-broker-instance.test.ts
+++ test/activemq/activemq-broker-instance.test.ts
@@ -240,4 +240,93 @@ describe("ActiveMqBrokerInstance", () => {
       ],
     });
   });
+
+	test("ActiveMQ Private Single Instance Broker Deployment with network components provided. Selection returns 2 subnets", () => {
+    const stack = new Stack();
+
+    const vpc = new Vpc(stack, "TestVpc",{
+			subnetConfiguration: [
+				{
+					cidrMask: 28,
+					name: "Private_1",
+					subnetType: SubnetType.PRIVATE_ISOLATED,
+				},
+				{
+					cidrMask: 28,
+					name: "Private_2",
+					subnetType: SubnetType.PRIVATE_ISOLATED,
+				},
+			]
+		});
+
+    const vpcSubnets: SubnetSelection = {
+      subnetType: SubnetType.PRIVATE_ISOLATED,
+    };
+
+    const broker = new ActiveMqBrokerInstance(stack, "TestBroker", {
+      publiclyAccessible: false,
+      version: ActiveMqBrokerEngineVersion.V5_18,
+      instanceType: InstanceType.of(InstanceClass.M5, InstanceSize.LARGE),
+      userManagement: ActiveMqBrokerUserManagement.simple({
+        users: [
+          {
+            username: "username",
+            password: SecretValue.unsafePlainText("password"),
+          },
+        ],
+      }),
+      autoMinorVersionUpgrade: false,
+      vpc,
+      vpcSubnets,
+    });
+
+    const template = Template.fromStack(stack);
+
+    // INFO: if weprovide the vpc/vpcSubnets - we have security group generated
+    expect(broker.connections).toBeDefined();
+
+    template.resourceCountIs("AWS::EC2::VPC", 1);
+
+    template.hasResourceProperties("AWS::EC2::VPC", {
+      CidrBlock: "10.0.0.0/16",
+      EnableDnsHostnames: true,
+      EnableDnsSupport: true,
+      InstanceTenancy: "default",
+    });
+
+    template.resourceCountIs("AWS::EC2::SecurityGroup", 1);
+
+    template.hasResourceProperties("AWS::EC2::SecurityGroup", {
+      GroupDescription: "Automatic security group for broker TestBroker",
+      SecurityGroupEgress: [
+        {
+          CidrIp: "255.255.255.255/32",
+          Description: "Disallow all traffic",
+          FromPort: 252,
+          IpProtocol: "icmp",
+          ToPort: 86,
+        },
+      ],
+      VpcId: {
+        Ref: "TestVpcE77CE678",
+      },
+    });
+
+    template.hasResourceProperties("AWS::AmazonMQ::Broker", {
+      AutoMinorVersionUpgrade: false,
+      BrokerName: "TestBroker",
+      DeploymentMode: "SINGLE_INSTANCE",
+      EncryptionOptions: { UseAwsOwnedKey: true },
+      EngineType: "ACTIVEMQ",
+      EngineVersion: "5.18",
+      HostInstanceType: "mq.m5.large",
+      PubliclyAccessible: false,
+      Users: [
+        {
+          Password: "password",
+          Username: "username",
+        },
+      ],
+    });
+  });
 });
