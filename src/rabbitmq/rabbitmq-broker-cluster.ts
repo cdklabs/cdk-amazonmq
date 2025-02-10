bea912ceceff4cd6a31f059137c72d3d54d8fe63
Rafał Pawłaszek <rpawlasz@amazon.com>
2025-02-10 09:36:52 +0100

    chore: preliminary implementation of the imports

--- src/rabbitmq/rabbitmq-broker-cluster.ts
+++ src/rabbitmq/rabbitmq-broker-cluster.ts
@@ -2,13 +2,19 @@
 Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: Apache-2.0
 */
+import { ISecurityGroup } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
 import { IRabbitMqBroker } from "./rabbitmq-broker";
 import {
+  IRabbitMqBrokerDeployment,
   RabbitMqBrokerDeploymentBase,
   RabbitMqBrokerDeploymentProps,
 } from "./rabbitmq-broker-deployment";
 import { BrokerDeploymentMode } from "../broker-deployment-mode";
+
+export interface IRabbitMqBrokerCluster
+  extends IRabbitMqBroker,
+    IRabbitMqBrokerDeployment {}
 
 export interface RabbitMqBrokerClusterProps
   extends RabbitMqBrokerDeploymentProps {}
@@ -18,8 +24,41 @@ export interface RabbitMqBrokerClusterProps
  */
 export class RabbitMqBrokerCluster
   extends RabbitMqBrokerDeploymentBase
-  implements IRabbitMqBroker
+  implements IRabbitMqBrokerCluster
 {
+  public static fromRabbitMqBrokerClusterArn(
+    scope: Construct,
+    id: string,
+    arn: string,
+    securityGroups?: ISecurityGroup[],
+  ) {
+    return RabbitMqBrokerCluster._fromRabbitMqBrokerDeploymentAttributes(
+      scope,
+      id,
+      arn,
+      undefined,
+      undefined,
+      securityGroups,
+    ) as IRabbitMqBrokerCluster;
+  }
+
+  public static fromRabbitMqBrokerClusterNameAndId(
+    scope: Construct,
+    logicalId: string,
+    name: string,
+    id: string,
+    securityGroups?: ISecurityGroup[],
+  ) {
+    return RabbitMqBrokerCluster._fromRabbitMqBrokerDeploymentAttributes(
+      scope,
+      logicalId,
+      undefined,
+      name,
+      id,
+      securityGroups,
+    ) as IRabbitMqBrokerCluster;
+  }
+
   constructor(scope: Construct, id: string, props: RabbitMqBrokerClusterProps) {
     super(scope, id, {
       ...props,
